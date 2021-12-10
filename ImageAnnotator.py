7d4e0db13da4a589749dcea081e9f31379911d76
Eva Radu <evaradu@hotmail.fr>
2021-12-10 16:50:35 +0100

    adding a window for category selection

--- ImageAnnotator.py
+++ ImageAnnotator.py
@@ -15,10 +15,17 @@ import glob
 first_coords = None
 second_coords = None
 
+boxes = []
+
+rect_id = 0
+
+
 # Fonction to handle the creation of boxes
 def click(event):
     global first_coords
     global second_coords
+    global boxes
+    global rect_id
 
     if(first_coords is None):  #ie if we didn't click a 1st time
         first_coords = (event.x,event.y)
@@ -27,7 +34,9 @@ def click(event):
         rect_id = canvas.create_rectangle(first_coords[0], first_coords[1], second_coords[0], second_coords[1],
                                   dash=(2,2), fill='', outline='black')
         canvas.coords(rect_id, first_coords[0], first_coords[1], second_coords[0], second_coords[1]) 
+        boxes = boxes + [[rect_id,first_coords,second_coords,"No Category"]]
         info_box(first_coords,second_coords)
+
 
         first_coords = None
 
@@ -35,7 +44,7 @@ def click(event):
 
 
 
-# Fonction to create a popup to tells information about a new box created. 
+# Fonction to create a popup to tells information about a box created. 
 def info_box(coords1, coords2):
    x1 = coords1[0]
    y1 = coords1[1]
@@ -49,17 +58,53 @@ def info_box(coords1, coords2):
    info_bot_right = "point at bottom right   : (" + str(max(x1,x2)) + " , " + str(min(y1,y2)) + ") \n"
    info_points = info_top_left + info_top_right + info_bot_left + info_bot_right
 
-   message = "You selected a new box with the following information : \n\n  " + info_points
+   message = "You selected a box with the following information : \n\n  " + info_points
 
    # Popup window : 
    popup= tk.Toplevel(root)
-   popup.geometry("410x190")
+   popup.geometry("410x230")
    popup.title("Information : New box")
    Label(popup, text= message).place(x=20,y=20)
 
    # Button to quit the popup window : 
    exit_button = tk.Button(popup, text="OK", command=popup.destroy)
-   exit_button.pack(pady=20 , side=tk.BOTTOM)
+   exit_button.pack(pady=5 , side=tk.BOTTOM)
+
+   # Button to select the category :
+   selection_button = tk.Button(popup, text = "Select a category",command=category_selection)
+   selection_button.pack(pady=5, side = tk.BOTTOM)
+
+
+# Function to update the category of the current box
+def update_category(choice):
+    global boxes
+    global rect_id
+
+    for i in boxes:
+        if i[0]==rect_id:
+            i[3]=choice
+
+    print(boxes)
+
+
+# Fonction to create a window to select the category of the current box
+def category_selection():
+    selection = tk.Toplevel(root)
+    selection.geometry("150x150")
+    selection.title("Category selection")
+
+    variable = tk.StringVar(selection)
+    variable.set("No Category") # default value
+
+    w = tk.OptionMenu(selection, variable, "No Category", "Category 1", "Category 2", "Category 3", command=update_category)
+    w.pack()
+
+    # Button to quit the window : 
+    exit_button = tk.Button(selection, text="OK", command=selection.destroy)
+    exit_button.pack(pady=5 , side=tk.BOTTOM)
+
+
+
 
 
 #########################
