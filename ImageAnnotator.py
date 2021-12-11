d5793fd267191479f2ba2516fac4cb0984145eb7
Eva Radu <evaradu@hotmail.fr>
2021-12-11 23:43:17 +0100

    adding the next image functionality

--- ImageAnnotator.py
+++ ImageAnnotator.py
@@ -2,30 +2,21 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import Label
 from PIL import Image, ImageTk
-
-path = "dataset/with_mask/image_101.png"
-'''
-img = Image.open(path)
-img.show()
-
 import glob
-#print(glob.glob("dataset/with_mask/*png"))
-'''
 
 first_coords = None
 second_coords = None
 
 boxes = {} # Dictionnary to stock the information of every box
 
-rect_id = 0
+rect_id = 0 # Number of the current selected rectangle
+
+current_image_number = 0 # Number of the current image
 
 
-# Fonction to handle the creation of boxes
+# Function to handle the creation of boxes
 def click(event):
-    global first_coords
-    global second_coords
-    global boxes
-    global rect_id
+    global first_coords, second_coords, boxes, rect_id, current_image_number
 
     if(first_coords is None):  #ie if we didn't click a 1st time
         first_coords = (event.x,event.y)
@@ -36,8 +27,13 @@ def click(event):
             for key,value in boxes.items():
                 x1 , y1 = value[0]
                 x2 , y2 = value[1]
-                if(((min(x1,x2)<=first_coords[0])and(first_coords[0]<=max(x1,x2)))
-                    and((min(y1,y2)<=first_coords[1])and(first_coords[1]<=max(y1,y2)))):  # ie if we are inside a box
+                if((((min(x1,x2)<=first_coords[0])
+                    and(first_coords[0]<=max(x1,x2)))
+                    and((min(y1,y2)<=first_coords[1])
+                    and(first_coords[1]<=max(y1,y2))))
+                    and(value[6]==current_image_number)):  # ie if we are inside a box
+
+                    rect_id = key
                     info_box(key)
 
         else: #Creation of a box
@@ -48,7 +44,7 @@ def click(event):
                 rect_id = canvas.create_rectangle(first_coords[0], first_coords[1], second_coords[0], second_coords[1],
                                           dash=(2,2), fill='', outline='black')
                 canvas.coords(rect_id, first_coords[0], first_coords[1], second_coords[0], second_coords[1])
-                boxes[rect_id]= [first_coords,second_coords,height,width,area,"No Category"] # we add a box in our dictionnary
+                boxes[rect_id]= [first_coords,second_coords,height,width,area,"No Category",current_image_number] # we add a box in our dictionnary
                 info_box(rect_id)
             else:
                 print("Be careful ! The box you are drawing is too small. Hence, it will not be saved.")
@@ -56,11 +52,11 @@ def click(event):
 
         first_coords = None
 
-    print("clicked at", event.x, event.y)
+    #print("clicked at", event.x, event.y)
 
 
 
-# Fonction to create a popup to tells information about a box created. 
+# Function to create a popup to tells information about a box created. 
 def info_box(rect_id):
    global boxes
    coords1 = boxes[rect_id][0]
@@ -105,18 +101,17 @@ def info_box(rect_id):
 
 # Function to update the category of the current box
 def update_category(choice):
-    global boxes
-    global rect_id
+    global boxes, rect_id
 
     #update of the category :
     info = boxes[rect_id]
     info[5] = choice
     boxes[rect_id] = info
 
-    print(boxes)
+    #print(boxes)
 
 
-# Fonction to create a window to select the category of the current box
+# Function to create a window to select the category of the current box
 def category_selection():
     selection = tk.Toplevel(root)
     selection.geometry("400x400")
@@ -133,8 +128,33 @@ def category_selection():
     exit_button.pack(pady=5 , side=tk.BOTTOM)
 
 
+# Function to update the image when we decide to go to the next image
+def update_image_right():
+    global current_image_number, rect_id, button1, button2
+
+    for key,value in boxes.items(): # Removing the draw of the boxes of the previous image
+        if(value[6]==current_image_number):
+            canvas.delete(key)
 
 
+    # next image
+    current_image_number += 1
+
+    if(current_image_number<=len(images_resized)): #If we still have images to annotate.
+        # change image on canvas
+        new_image = images_resized[current_image_number]
+        canvas.config(width=new_image.width(), height=new_image.height())
+        canvas.itemconfig(image_id, image=new_image)
+
+    else: # If there are no more images to annotate.
+        canvas.delete("all")
+
+        messagebox.showinfo("Information", "You annoted every images.")
+
+        button1.destroy()
+        button2.destroy()
+
+    
 
 #########################
 ###    MAIN WINDOW    ###
@@ -143,32 +163,51 @@ def category_selection():
 root = tk.Tk()
 root.wm_title("Tkinter window")
 root.geometry("1000x1000")
-img = ImageTk.PhotoImage(Image.open(path))
 
+img = ImageTk.PhotoImage(Image.open("dataset/with_mask/image_0.png"))
 
 canvas= tk.Canvas(root, width=img.width(), height=img.height(),
                    borderwidth=0, highlightthickness=0)
 canvas.pack(expand=True)
-canvas.create_image(0, 0, image=img, anchor=tk.NW)
+
+
+print("Generation of the images ...")
+print("It might take a few seconds.")
+
+list_images = glob.glob("dataset/with_mask/*png") + glob.glob("dataset/without_mask/*png")  # All the paths
+
+images = [Image.open(i) for i in list_images] #All the images
+
+for i in range(len(images)):
+    ig = images[i]
+    width_ig, height_ig = ig.size
+
+    if(width_ig > 500 or height_ig >500): # ie if an image is too big ==> We need to resize it.
+        scale = max(width_ig,height_ig)/500
+        height_ig = int(height_ig / scale)
+        width_ig = int(width_ig / scale)
+        images[i] = ig.resize((width_ig,height_ig))
+
+images_resized = [ImageTk.PhotoImage(i) for i in images] #All the resized images
+
+print("All the images have been generated.")
+
+image_id = canvas.create_image(0, 0, anchor='nw', image=images_resized[current_image_number])
 
 
 canvas.bind("<Button-1>", click)
 canvas.pack()
 
 
+
 #########################
 ###      BUTTONS      ###
 #########################
-button1 = tk.Button(root, text="-->", bg='white')
-button1.pack(side=tk.BOTTOM)
+button1 = tk.Button(root, text="NEXT PICTURE\n-->", bg='white',command=update_image_right)
+button1.pack(pady=5,side=tk.RIGHT)
 
-button2 = tk.Button(root, text="<--", bg='white')
-button2.pack(side=tk.BOTTOM)
-
-button3 = tk.Button(root, text="Save", bg='white')
-button3.pack(side=tk.BOTTOM)
-
-
+button2 = tk.Button(root, text="SAVE", bg='white')
+button2.pack(pady=5,side=tk.BOTTOM)
 
 
 # show window
