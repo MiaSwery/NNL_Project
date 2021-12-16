c75d64c605db03b6eab094df7a6372a6baa3eadc
Eva Radu <evaradu@hotmail.fr>
2021-12-16 22:38:23 +0100

    adding a welcome page and a json file

--- ImageAnnotator.py
+++ ImageAnnotator.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import Label
 from PIL import Image, ImageTk
+import json
 import glob
 
 first_coords = None
@@ -44,7 +45,7 @@ def click(event):
             area  = height*width
             if (area>40) and (width>5) and (height>5) :
                 rect_id = canvas.create_rectangle(first_coords[0], first_coords[1], second_coords[0], second_coords[1],
-                                          dash=(2,2), fill='', outline='black')
+                                          dash=(2,2), fill='', outline='red')
                 canvas.coords(rect_id, first_coords[0], first_coords[1], second_coords[0], second_coords[1])
                 boxes[rect_id]= [first_coords,second_coords,height,width,area,"No Category",current_image_number] # we add a box in our dictionnary
                 info_box(rect_id)
@@ -127,7 +128,7 @@ def update_category(choice):
 def new_category():
     global entry , list_category
     input = entry.get() 
-    if((input not in list_category)and(len(input.strip()))):
+    if((input not in list_category)and(len(input.strip()))and(input!='Enter the name of the new category')):
         list_category.append(input)
         messagebox.showinfo("Information", "You added the following category : " + input)
 
@@ -197,7 +198,7 @@ def category_selection():
 
 # Function to update the image when we decide to go to the next image
 def update_image_right():
-    global current_image_number, rect_id, button1, button2
+    global current_image_number, rect_id, button1, button2,canvas
 
     for key,value in boxes.items(): # Removing the draw of the boxes of the previous image
         if(value[6]==current_image_number):
@@ -206,7 +207,7 @@ def update_image_right():
     # next image
     current_image_number += 1
 
-    if(current_image_number<len(list_images)): #If we still have images to annotate.
+    if(current_image_number<len(list_path)): #If we still have images to annotate.
         # change image on canvas
         new_image = images_resized[current_image_number]
         canvas.config(width=new_image.width(), height=new_image.height())
@@ -214,12 +215,19 @@ def update_image_right():
 
     else: # If there are no more images to annotate.
         canvas.delete("all")
+        generate_json()
 
-        messagebox.showinfo("Information", "You annoted every images.")
+        messagebox.showinfo("Information", "You annoted every images.\nThe json file have been generated.")
 
         button1.destroy()
         button2.destroy()
+        canvas.destroy()
 
+
+
+##################################
+###  FUNCTIONS FOR THE ENTRIES ###   
+##################################
 
 def on_entry_click_AddCategory(event):
     global entry
@@ -257,60 +265,132 @@ def function_focusout(text_entry, text):
         text_entry.insert(0, text)
         text_entry.config(fg = 'grey')
 
+########################
+###  GENERATION JSON ###   
+########################
+
+# Function to get all the boxes of an image and convert them into a dictionnary 
+def convert_image_to_json(path,id):
+    global boxes
+
+    data_image = []
+    for key,value in boxes.items():
+        if(value[6]==id):
+            data_image.append(convert_box_to_json(value[2],value[3],value[4],value[5]))
+    if(data_image == []):
+        return "null"
+
+    else:
+        return data_image
+            
+        
+# Function to convert the information of a single box into a dictionnary
+def convert_box_to_json(height,width,area,category):
+    # Rajouter les coordonnées des points ??? 
+    return {'height' : height, 'width' : width, 'area' : area, 'category' : category}
+
+# Function to generate the whole json
+def generate_json():
+    global list_path
+    # Data to be written
+    data_json = []
+
+    for i in range(len(list_path)):
+        data_json.append({'image':{'path' : list_path[i], 'boxes' : convert_image_to_json(list_path[i],i)}})
+      
+    # Serializing json 
+    json_object = json.dumps({'data': data_json}, indent = 4)
+      
+    # Writing to sample.json
+    with open("annotations.json", "w") as outfile:
+        outfile.write(json_object)     
+
 
 #########################
 ###    MAIN WINDOW    ###
 #########################
-
 root = tk.Tk()
-root.wm_title("Tkinter window")
+root.wm_title("Image Annotator")
 root.geometry("1000x1000")
-
-img = ImageTk.PhotoImage(Image.open("dataset/with_mask/image_0.png"))
-
-canvas= tk.Canvas(root, width=img.width(), height=img.height(),
-                   borderwidth=0, highlightthickness=0)
-canvas.pack(expand=True)
+#root.configure(background='pink')
 
 
-print("Generation of the images ...")
-print("It might take a few seconds.")
+welcome = "Welcome\n"
+msg = 'Press the button "Start" when you are ready to annotate the images. \n\n' 
+msg = msg + 'Note : After pressing the button "Start", the images might take a few seconds to be generated. \n\n'
+msg = msg + 'A revoir'
 
-list_images = glob.glob("dataset/with_mask/*png") + glob.glob("dataset/without_mask/*png")  # All the paths
-list_images = list_images[0:7] 
-images = [Image.open(i) for i in list_images] #All the images
+welcome_message = tk.Text(root, height = 35, width = 300)
+welcome_message.insert('end', welcome+'\n')
+welcome_message.insert('end', msg+'\n')
 
-for i in range(len(images)):
-    ig = images[i]
-    width_ig, height_ig = ig.size
-
-    if(width_ig > 500 or height_ig >500): # ie if an image is too big ==> We need to resize it.
-        scale = max(width_ig,height_ig)/500
-        height_ig = int(height_ig / scale)
-        width_ig = int(width_ig / scale)
-        images[i] = ig.resize((width_ig,height_ig))
-
-images_resized = [ImageTk.PhotoImage(i) for i in images] #All the resized images
-
-print("All the images have been generated.")
-
-image_id = canvas.create_image(0, 0, anchor='nw', image=images_resized[current_image_number])
+welcome_message.tag_add('welcome','1.0','1.end')
+welcome_message.tag_config('welcome', font='comic 20 bold')  
+welcome_message.tag_configure("welcome", justify='center')
 
 
-canvas.bind("<Button-1>", click)
-canvas.pack()
+welcome_message.tag_add('msg','2.0','2.end')
+welcome_message.tag_config('msg', font='arial 13 normal')
+
+welcome_message.config(state= tk.DISABLED)
+welcome_message.pack()
+
+
+# Function called after pressing the button "Start"
+def start_app():
+    global image_id, canvas, images_resized ,   list_path, button1, button2
+    button_start.destroy()
+
+    img = ImageTk.PhotoImage(Image.open("dataset/with_mask/image_0.png"))
+
+    canvas= tk.Canvas(root, width=img.width(), height=img.height(),
+                       borderwidth=0, highlightthickness=0)
+    #canvas.configure(background = "pink")
 
 
 
-#########################
-###      BUTTONS      ###
-#########################
-button1 = tk.Button(root, text="NEXT PICTURE\n-->", bg='white',command=update_image_right)
-button1.pack(pady=5,side=tk.RIGHT)
+    #print("Generation of the images ...")
+    #print("It might take a few seconds.")
 
-button2 = tk.Button(root, text="SAVE", bg='white')
-button2.pack(pady=5,side=tk.BOTTOM)
+    list_path = glob.glob("dataset/with_mask/*png") + glob.glob("dataset/without_mask/*png")  # All the paths
+    list_path = list_path[0:7] 
+    images = [Image.open(i) for i in    list_path] #All the images
 
+    for i in range(len(images)):
+        ig = images[i]
+        width_ig, height_ig = ig.size
+
+        if(width_ig > 500 or height_ig >500): # ie if an image is too big ==> We need to resize it.
+            scale = max(width_ig,height_ig)/500
+            height_ig = int(height_ig / scale)
+            width_ig = int(width_ig / scale)
+            images[i] = ig.resize((width_ig,height_ig))
+
+    images_resized = [ImageTk.PhotoImage(i) for i in images] #All the resized images
+
+    #print("All the images have been generated.")
+
+    image_id = canvas.create_image(0, 0, anchor='nw', image=images_resized[current_image_number])
+
+
+    canvas.bind("<Button-1>", click)
+    canvas.pack(expand=True)
+
+
+
+    #########################
+    ###      BUTTONS      ###
+    #########################
+    button1 = tk.Button(root, text="NEXT PICTURE\n-->", bg='white',command=update_image_right)
+    button1.pack(pady=5,side=tk.RIGHT)
+
+    button2 = tk.Button(root, text="SAVE", bg='white')
+    button2.pack(pady=5,side=tk.BOTTOM)
+
+
+
+button_start = tk.Button(root, text="START", bg='white',command=lambda:[welcome_message.destroy(), start_app()])
+button_start.pack(pady=5,side=tk.BOTTOM)
 
 # show window
 root.mainloop()
